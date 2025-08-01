f3d809247ac40e13e601920e3c1e071e2765c31e
Sabyasachee Thakur <thakursabya7@gmail.com>
2025-08-01 21:58:03 +0530

    Hosted psql

--- internal/db/db.go
+++ internal/db/db.go
@@ -24,17 +24,9 @@ func InitDB() {
 	if errs != nil {
 		log.Fatal("Error loading .env file")
 	}
-
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
-	)
-
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	dsn := os.Getenv("DATABASE_URL")
+	var err error
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to DATABASE: %v", err)
 
